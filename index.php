ba742ffcb9b3df6bb9e5c1b2acf3d0836d8f0c8c
Silvio Cruz <silviocruzdeveloper@gmail.com>
2024-04-01 01:57:19 -0400

    Tirando bug

--- index.php
+++ index.php
@@ -6,60 +6,94 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body {
-            font-size: 25px;
-            color: #fff;
-            background-color: #000;
-        }
+    body {
+        font-size: 25px;
+        color: #fff;
+        background-color: #000;
+    }
 
-        span {
-            color: #000;
-        }
+    span {
+        color: #000;
+    }
+
+    .botao {
+        margin: 5px auto;
+        padding: auto;
+        background-color: #ccc;
+        color: #000;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        cursor: pointer;
+        box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+    }
+
+    .botao:hover {
+        background-color: #999;
+    }
+
+    .botao:active {
+        background-color: #666;
+        box-shadow: none;
+    }
+
+    div {
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
     </style>
 </head>
 
 <body>
-
-
+    <div><button onclick="redirect()" class="botao" id="btn">Chame a vaca</button></div>
+    <!-- Exibição da mensagem 'cowsay' -->
+    <br>
+    <br>
     <?php
-    $cow = $_GET['cow'];
-    if ($cow == "cowsay") : ?>
-        / You have Egyptian flu: you\'re going to \
-        <br>
-        \ be a mummy. <span>____________________</span> /
-        <br>
-        <span>_</span>-----------------------------------------------
-        <br>
-        <span>______</span>\<span>___</span> ^__^
-        <br>
-        <span>______</span> \ <span>___</span>(oo)\_______
-        <br>
-        <span>__________</span> (__)\<span>_______</span> )\/\
-        <br>
-        <span>_______________</span>||- - - -w |
-        <br>
-        <span>_______________</span>||<span>______</span> ||
-    <?php elseif ($cow == []) : ?>
-        ?????????
-    <?php endif ?>
-
-
+    // Verifica se o parâmetro 'cow' foi passado pela URL
+    if (isset($_GET['cow'])) {
+        $cow = $_GET['cow'];
+        if ($cow == "cowsay") {
+            // Exibe a mensagem 'cowsay'
+            echo '
+            / You have Egyptian flu: you\'re going to \
+            <br>
+            \ be a mummy. <span>____________________</span> /
+            <br>
+            <span>_</span>-----------------------------------------------
+            <br>
+            <span>______</span>\<span>___</span> ^__^
+            <br>
+            <span>______</span> \ <span>___</span>(oo)\_______
+            <br>
+            <span>__________</span> (__)\<span>_______</span> )\/\
+            <br>
+            <span>_______________</span>||- - - -w |
+            <br>
+            <span>_______________</span>||<span>______</span> ||
+            ';
+        } else {
+            // Exibe uma mensagem padrão se 'cow' não for 'cowsay'
+            echo '?????????';
+        }
+    }
+    ?>
 </body>
+
 <script>
+function redirect() {
     var cow = prompt("Digite o nome da vaca:");
-    document.getElementById("cow").innerHTML = cow;
-
-    // Codificar o valor de 'cow' para garantir que ele esteja seguro para uma URL
     var cowEncoded = encodeURIComponent(cow);
-
-    // Construir a URL com o parâmetro 'cow'
     var url = "index.php?cow=" + cowEncoded;
-
-    // Redirecionar para a nova URL
     window.location.href = url;
+
+}
 </script>
-
-
-
 
 </html>
